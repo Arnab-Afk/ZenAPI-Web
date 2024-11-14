5f69fe3cf10990e3cfca80939f99e038613253af
Arnab-Afk <arnabbhowmik019@gmail.com>
2024-11-14 15:09:13 +0530

    samity

--- sanity.cli.js
+++ sanity.cli.js
@@ -1,6 +1,6 @@
 import { defineCliConfig } from 'sanity/cli'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+const projectId = uw78hfyk
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
 
 export default defineCliConfig({ api: { projectId, dataset } })
